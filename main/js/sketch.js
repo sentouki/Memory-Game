ab349407239b1827c4f19f8f2f6204518e26941e
Tim Thomas <tim-t@hotmail.de>
2021-09-01 00:31:10 +0200

    move card class into seperate file

--- main/js/sketch.js
+++ main/js/sketch.js
@@ -1,4 +1,5 @@
 let tiles = []
+let images = []
 
 const numRows = 4
 const numColumns = 3
@@ -6,61 +7,9 @@ const numColumns = 3
 const mcardWidth = 100
 const mcardHeight = 150
 
+const rotateSpeed = 5
+const rotateLevitation = 0.6
 
-// basic card class which represents card with two sides
-class Card {
-  constructor(x,y, upperSideColor=[93,81,124],downSideColor=[60,60,60], stroke=[255,255,255]) {
-    this.x = x;
-    this.y = y;
-    this.upColor = upperSideColor;
-    this.downColor = downSideColor;
-    this.stroke = stroke;
-    this.faceUp = false;
-    this.rot = 0;
-  }
-  render() {
-    push();
-    // moving the top left card in the top left corner because of "rectMode(CENTER)"
-    translate(((-width/2)+mcardWidth/2)+this.x,((-height/2)+mcardHeight/2)+this.y, 0);
-    fill(this.upColor);
-    stroke(this.stroke);
-    // rotateY(-millis()/500);
-    // upper side
-    if (this.faceUp){
-      if (this.rot < 180) {
-        this.rot += 5;
-      }
-      rotateY(this.rot);
-      if (this.rot < 90 && this.rot > 0) {
-        this.y -= 0.6;
-      }
-      else if (this.rot > 90 && this.rot < 180) {
-        this.y += 0.6;
-      }
-    }
-    else {
-      if (this.rot > 0) {
-        this.rot -= 5;
-      }
-      rotateY(-this.rot);
-      if (this.rot < 90 && this.rot > 0) {
-        this.y += 0.6;
-      }
-      else if (this.rot > 90 && this.rot < 180) {
-        this.y -= 0.6;
-      }
-    }
-    rect(0,0,mcardWidth, mcardHeight);
-    // down side
-    fill(this.downColor);
-    translate(0,0,-1);
-    rect(0,0,mcardWidth, mcardHeight);
-    pop();
-  }
-  turn() {
-    this.faceUp = !this.faceUp;
-  }
-}
 
 const testCard = new Card(0,0);
 
@@ -73,6 +22,13 @@ class MemoryCard {
 
 }
 
+// load images before setup 
+function preload() {
+  // load same number of images as cards into array
+  // while (images.length < numRows * numColumns) {
+
+  // } 
+}
 
 function setup() {
   createCanvas(520, 550, WEBGL);
@@ -93,9 +49,8 @@ function draw() {
 function mouseClicked() {
   //console.log(mouseX,mouseY);
   tiles.forEach(tile => {
-    console.log(tile.x, tile.y)
     if ((mouseX > tile.x && mouseX < tile.x + 100) && mouseY > tile.y && mouseY < tile.y + 150)  {
-        console.log("Yeehaw:" + tile.x)
+        // console.log("Yeehaw:" + tile.x)
       tile.turn();
         }
   })
